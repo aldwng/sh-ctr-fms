ea91682f52e86a8f749e899da38dd46f19c2f18f
lingdawang <lingdawang@sohu-inc.com>
2023-09-21 16:52:44 +0800

    Add new layers.

---
+++ core/layers.py
@@ -0,0 +1,82 @@
+import numpy as np
+import torch
+
+class FMLinear(torch.nn.Module):
+    
+    def __init__(self, field_dims, output_dim):
+        super().__init__()
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim)))
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return torch.sum(self.fc(x), dim=1) + self.bias
+    
+
+class FMEmbedding(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return self.embedding(x)
+    
+
+class FactoMachine(torch.nn.Module):
+
+    def __ini__(self, reduce_sum=True):
+        super().__init__()
+        self.reduce_sum = reduce_sum
+
+    def forward(self, x):
+        square_of_sum = torch.sum(x, dim=1) ** 2
+        sum_of_square = torch.sum(x ** 2, dim=1)
+        ix = square_of_sum - sum_of_square
+        if self.reduce_sum:
+            ix = torch.sum(ix, dim=1, keepdim=True)
+        return 0.5 * ix
+    
+class InnerProduct(torch.nn.Module):
+
+    def forward(self, x):
+        num_fields = x.shape[1]
+        row, col = list(), list()
+        for i in range(num_fields - 1):
+            for j in range(i + 1, num_fields):
+                row.append(i), col.append(j)
+        return torch.sum(x[:, row] * x[:, col], dim=2)
+    
+class OuterProduct(torch.nn.Module):
+
+    def __ini__(self, num_fields, embed_dim, kernel_type='mat'):
+        super().__init__()
+        num_ix = num_fields * (num_fields - 1)
+        if kernel_type == 'mat':
+            kernel_shape = embed_dim, num_ix, embed_dim
+        elif kernel_type == 'vec':
+            kernel_shape = num_ix, embed_dim
+        elif kernel_type == 'num':
+            kernel_shape = num_ix, 1
+        else:
+            raise ValueError('unknown kernel type: ', kernel_type)
+        self.kernel_type = kernel_type
+        self.kernel = torch.nn.Parameter(torch.zeros(kernel_shape))
+        torch.nn.init.xavier_uniform_(self.kernel.data)
+
+    def forward(self, x):
+        num_fields = x.shape[1]
+        row, col = list(), list()
+        for i in range(num_fields - 1):
+            for j in range(i + 1, num_fields):
+                row.append(i), col.append(j)
+        p, q = x[: row], x[:, col]
+        if self.kernel_type == 'mat':
+            kp = torch.sum(p.unsqeeze(1) * self.kernel, dim=1).permute(0, 2, 1)
+            return torch.sum(kp * q, -1)
+        else:
+            return torch.sum(p * q * self.kernel.unsqueeze(0), -1)
